e52fb6777a4b26366ed327bd8df5a210e0056d9d
Jeffrey Ruder <ruderjt@gmail.com>
2016-01-06 19:53:00 -0800

    Correct misnamed class in IntegrationTest.java

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,8 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+  //Tests go here
+
+}
--- src/test/java/IntegrationTest.java
+++ src/test/java/IntegrationTest.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class AppTest extends FluentTest {
+public class IntegrationTest extends FluentTest {
   public WebDriver webDriver = new HtmlUnitDriver();
   public WebDriver getDefaultDriver() {
     return webDriver;
