5cce1dd05f1032a905e96ab5033c106edd2807f1
Jeffrey Ruder <ruderjt@gmail.com>
2016-01-22 09:21:13 -0800

    Add basic CRUD functionality to Client class

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -5,13 +5,16 @@ public class Client {
   private String mFirstName;
   private String mLastName;
   private int mId;
-  private int mStylistId;
+
+  //CONSTRUCTOR
 
   public Client(String first, String last) {
-    mFirstName = first;
-    mLastName = last;
+    mFirstName = first.trim();
+    mLastName = last.trim();
   }
-  
+
+  //GETTERS
+
   public String getFirstName() {
     return mFirstName;
   }
@@ -26,5 +29,86 @@ public class Client {
 
   public int getId() {
     return mId;
+  }
+
+  //EQUALITY
+
+  @Override
+  public boolean equals(Object otherClient) {
+    if (!(otherClient instanceof Client)) {
+      return false;
+    } else {
+      Client client = (Client) otherClient;
+      return this.getFullName().equals(client.getFullName());
+    }
+  }
+
+  //CREATE
+
+  public void save() {
+    String sql = "INSERT INTO clients(first_name, last_name) VALUES (:first, :last)";
+    try (Connection con = DB.sql2o.open()) {
+      this.mId = (int) con.createQuery(sql, true)
+        .addParameter("first", this.getFirstName())
+        .addParameter("last", this.getLastName())
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  //READ
+
+  public static List<Client> all() {
+    String sql = "SELECT id AS mId, first_name AS mFirstName, last_name AS mLastName FROM clients";
+    try (Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql)
+        .executeAndFetch(Client.class);
+    }
+  }
+
+  public static List<Client> all(boolean alphabetical) {
+    if (alphabetical) {
+      List<Client> clients = Client.all();
+      clients.sort((c1, c2) -> c1.mLastName.compareTo(c2.mLastName));
+      return clients;
+    } else {
+      return Client.all();
+    }
+  }
+
+  public static Client find(int search) {
+    String sql = "SELECT id AS mId, first_name AS mFirstName, last_name AS mLastName FROM clients WHERE id = :id";
+    try (Connection con = DB.sql2o.open()) {
+      Client client = (Client) con.createQuery(sql)
+        .addParameter("id", search)
+        .executeAndFetchFirst(Client.class);
+      return client;
+    }
+  }
+
+  //UPDATE
+
+  public void update(String first, String last) {
+    String sql = "UPDATE clients SET first_name = :first, last_name = :last WHERE id = :id";
+    mFirstName = first;
+    mLastName = last;
+    try (Connection con = DB.sql2o.open()) {
+      con.createQuery(sql)
+        .addParameter("first", first)
+        .addParameter("last", last)
+        .addParameter("id", this.getId())
+        .executeUpdate();
+    }
+  }
+
+  //DELETE
+
+  public void delete() {
+    String sql = "DELETE FROM clients WHERE id = :id";
+    try (Connection con = DB.sql2o.open()) {
+      con.createQuery(sql)
+        .addParameter("id", this.getId())
+        .executeUpdate();
+    }
   }
 }
--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -14,4 +14,60 @@ public class ClientTest {
     assertEquals("Jane Roe", client.getFullName());
   }
 
+  @Test
+  public void equals_returnsTrueIfSameName() {
+    Client firstClient = new Client("John", "Doe");
+    Client secondClient = new Client("John", "Doe");
+    assertTrue(firstClient.equals(secondClient));
+  }
+
+  @Test
+  public void save_savesClientInDatabase() {
+    Client client = new Client("John", "Doe");
+    client.save();
+    assertEquals(client.getFullName(), Client.find(
+      client.getId()).getFullName());
+  }
+
+  @Test
+  public void all_returnsAllClients() {
+    Client firstClient = new Client("John", "Doe");
+    Client secondClient = new Client("Jane", "Roe");
+    firstClient.save();
+    secondClient.save();
+    Client[] clients = new Client[] {firstClient, secondClient};
+    assertTrue(Client.all().containsAll(Arrays.asList(clients)));
+  }
+
+  @Test
+  public void allWithTrue_returnsOrderedListOfAllClients() {
+    Client firstClient = new Client("John", "Doe");
+    Client secondClient = new Client("Jane", "Roe");
+    Client thirdClient = new Client("Hank", "Aaron");
+    firstClient.save();
+    secondClient.save();
+    thirdClient.save();
+    assertTrue(Client.all(true).get(0).equals(thirdClient));
+  }
+
+  @Test
+  public void update_changesClientNameInObjectAndDatabase() {
+    Client client = new Client("John", "Doe");
+    client.save();
+    client.update("Max", "Moe");
+    assertEquals("Max Moe", client.getFullName());
+    assertEquals("Max Moe", Client.find(
+      client.getId()).getFullName());
+  }
+
+  @Test
+  public void delete_removesClientFromDatabase() {
+    Client firstClient = new Client("John", "Doe");
+    Client secondClient = new Client("Jane", "Roe");
+    firstClient.save();
+    secondClient.save();
+    firstClient.delete();
+    assertTrue(!(Client.all().contains(firstClient)));
+  }
+
 }
