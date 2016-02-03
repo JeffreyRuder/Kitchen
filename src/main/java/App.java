8047f4a32c64761bba37c7bf51536322a843c95a
Jeffrey Ruder <ruderjt@gmail.com>
2016-02-02 16:07:33 -0800

    Add route to accept delivery

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -116,6 +116,17 @@ public class App {
       return new ModelAndView(model, layout);
       }, new VelocityTemplateEngine());
 
+    post("/manager/new-ingredient", (request, response) -> {
+      Ingredient newIngredient = new Ingredient(
+        request.queryParams("name"),
+        request.queryParams("unit"),
+        Integer.parseInt(request.queryParams("desired-on-hand")),
+        Integer.parseInt(request.queryParams("shelf-life-days")));
+      newIngredient.save();
+      response.redirect("/manager/ingredients/" + newIngredient.getId());
+      return null;
+    });
+
     get("/manager/new-ingredient", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/ingredient-new.vtl");
@@ -130,6 +141,19 @@ public class App {
       model.put("template", "templates/ingredients-inventory.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
+    //Take a delivery
+    post("/manager/ingredients/inventory", (request, response) -> {
+      for (Ingredient ingredient : Ingredient.all()) {
+        int amount = Integer.parseInt(request.queryParams(ingredient.getName()));
+        if (amount > 0) {
+          Inventory delivery = new Inventory(ingredient.getId(), amount);
+          delivery.save();
+        }
+      }
+      response.redirect("/manager/inventory");
+      return null;
+    });
 
     get("/manager/delivery", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
